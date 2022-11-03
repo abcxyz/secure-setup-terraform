718f6f86dad2e6827a8cb12d798c1d3f5ecfb856
Brad Egler <begler@gmail.com>
2022-11-03 09:31:52 -0400

    feat: updated to capture all terraform checksums upfront. Changed the action to pull the updated checksum file rather than passing the checksum as a parameter

---
+++ generate_version_checksums.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+mkdir temp
+cd temp || exit
+
+export GNUPGHOME=./.gnupg;
+
+checksum_file=../secure-setup-terraform-checksums.json
+
+# Generate a temporary key to use for verification
+gpg --batch --quick-generate-key --batch --passphrase "" github-action@abcxyz.dev;
+
+# Retrieve the hashicorp key
+curl -s --remote-name https://keybase.io/hashicorp/pgp_keys.asc;
+
+# Import the key from hashicorp
+gpg --batch --import pgp_keys.asc;
+
+# Sign the hashicorp key with our key
+gpg --batch --yes --trust-model always --sign-key 34365D9472D7468F;
+
+release_url=https://releases.hashicorp.com/terraform;
+
+curl -s --remote-name ${release_url}/index.json;
+
+# Exclude all 0.x and pre-release versions
+jq -r 'select(.name=="terraform") | .versions[] | select(.version | (contains("-") or startswith("0.")) | not) | .version' < index.json > versions.list;
+
+while IFS= read -r version; 
+do 
+    exists=$(jq --arg version "${version}" '.versions[] | select(.version==$version)' < "${checksum_file}");
+    if [ "${exists}" = "" ]; 
+    then
+        version_file=${version}.json;
+        jq -r --arg version "${version}" 'select(.name=="terraform") | .versions[] | select(.version==$version)' < index.json > "${version_file}";
+        sha_file=$(jq -r '.shasums' < "${version_file}");
+        sig_file=$(jq -r '.shasums_signature' < "${version_file}");
+        curl -s --remote-name "${release_url}/${version}/${sha_file}";
+        curl -s --remote-name "${release_url}/${version}/${sig_file}";
+
+        # Iterate over both supported architectures
+        set -- amd64 arm64;
+        for arch in "$@";
+        do
+            bin_url=$(jq -r --arg arch "${arch}" '.builds[] | select(.os=="linux" and .arch==$arch) | .url' < "${version_file}");
+            bin_file=$(jq -r --arg arch "${arch}" '.builds[] | select(.os=="linux" and .arch==$arch) | .filename' < "${version_file}");
+            # Download the archive, sha file and signature
+            curl -s --remote-name "${bin_url}"
+
+            # Verify the signature against the sha file
+            gpg --batch --verify "${sig_file}" "${sha_file}";
+
+            # Verify the archive's checksum
+            shasum --algorithm 256 --check --ignore-missing "${sha_file}";
+
+            # Extract the binary from the archive
+            unzip -qq -o "${bin_file}";
+
+            # Extract only the shasum for the archive we care about
+            arch_sum=$(grep "${bin_file}" "${sha_file}" | cut -d' ' -f1);
+
+            # Produce a checksum of the binary
+            bin_sum=$(shasum -a 256 terraform | cut -d' ' -f1);
+
+            version_info=$(jq -c -n --arg version "${version}" --arg archive_checksum "${arch_sum}" --arg binary_checksum "${bin_sum}" --arg os "linux" --arg arch "${arch}" '$ARGS.named');
+            jq -c --argjson version_info "${version_info}" '.versions += [$version_info]' < "${checksum_file}" > updated.json;
+            mv updated.json "${checksum_file}";
+
+        done;
+    fi
+
+done < versions.list;
+
+unset GNUPGHOME;

07e27af2369be8a26b858cdc2ab16c432c4b23ed
Dylan Yurgionas <dcreey@gmail.com>
2025-04-21 21:08:11 +0000

    fix: lint problems (#158)
    Temporarily adds the lint.yml from abcxyz/actions, then fixes lint
    problems, then removes the lint.yml. This repo will be added to the org
    ruleset so it gets linted after this merges.
    
    ---------
    
    Signed-off-by: Dylan Yurgionas <dcreey@gmail.com>
    Co-authored-by: Seth Vargo <seth@sethvargo.com>

--- .github/generate_version_checksums.sh
+++ .github/generate_version_checksums.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env/bash
 
-checksum_file=$1
-if [ "${checksum_file}" = "" ];
-then
+set -euo pipefail
+
+# Expected to be run in github actions context.
+declare -r GITHUB_ENV
+
+checksum_file="${1}"
+if [[ -n "${checksum_file}" ]]; then
     echo "Usage: $0 <path to checkums file>";
     exit 1;
 fi;
@@ -26,7 +30,7 @@ gpg --batch --yes --trust-model always --sign-key 34365D9472D7468F;
 
 release_url=https://releases.hashicorp.com/terraform;
 
-curl -s --remote-name ${release_url}/index.json;
+curl -s --remote-name "${release_url}/index.json";
 
 # Exclude all 0.x and pre-release versions
 jq -r 'select(.name=="terraform") | .versions[] | select(.version | (contains("-") or startswith("0.")) | not) | .version' < index.json > versions.list;
@@ -39,8 +43,7 @@ touch "${added_file}";
 while IFS= read -r version; 
 do 
     exists=$(jq --arg version "${version}" '.versions[] | select(.version==$version)' < "${checksum_file}");
-    if [ "${exists}" = "" ]; 
-    then
+    if [[ "${exists}" = "" ]]; then
         version_file=${version}.json;
         jq -r --arg version "${version}" 'select(.name=="terraform") | .versions[] | select(.version==$version)' < index.json > "${version_file}";
         sha_file=$(jq -r '.shasums' < "${version_file}");
@@ -84,14 +87,14 @@ do
 done < versions.list;
 
 # If there were any changes set some environment variables
-if [ -s ${added_file} ]; 
-then
+if [[ -s "${added_file}" ]]; then
     version_file="../VERSION"
     action_file="../action.yml"
 
-    change_count=$(wc -l ${added_file} | tr -s ' ' | cut -d ' ' -f1);
+    change_count=$(wc -l "${added_file}" | tr -s ' ' | cut -d ' ' -f1);
     change_date=$(date +%Y-%m-%d);
-    versions=$(cat ${added_file} | tr '\n' ',' | sed 's/,*$//g');
+    # shellcheck disable=SC2002
+    versions=$(cat "${added_file}" | tr '\n' ',' | sed 's/,*$//g');
 
     release_version=$(awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}' <"${version_file}")
     sed -i "s/RELEASE_VERSION: '.*'/RELEASE_VERSION: '${release_version}'/g" "${action_file}"
